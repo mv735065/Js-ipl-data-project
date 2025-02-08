0ff730e2e8e012e6094c6475bb8586ae1fa3c74f
mv735065 <245119735065@mvsrec.edu.in>
2025-02-08 12:30:02 +0530

    5th problem, changed map to obj

--- src/Server/5-won-toss-and-match.js
+++ src/Server/5-won-toss-and-match.js
@@ -1,30 +1,23 @@
 // Find the number of times each team won the toss and also won the match
 
+const matchData = require("../Data/matches.json");
 
-const matchData = require('../Data/matches.json');
+const fs = require("fs");
 
-const fs=require('fs')
+let teamsWonBoth = {};
 
-let map=new Map();
+matchData.forEach((match) => {
+  let toss_winner = match.toss_winner;
+  let match_winner = match.winner;
 
+  if (toss_winner !== match_winner) return;
 
-matchData.forEach((match)=>{
-    let toss_winner=match.toss_winner;
-    let match_winner=match.winner;
-    if(toss_winner===match_winner){
-    if(map.has(match_winner)){
-       map.set(match_winner,map.get(match_winner)+1);
-    }else{
-        map.set(match_winner,1);
-    }
-}
+  if (!teamsWonBoth.hasOwnProperty(match_winner)) {
+    teamsWonBoth[match_winner] = 0;
+  }
+  teamsWonBoth[match_winner] += 1;
+});
 
- });
-
- console.log(map);
-
- let res=Object.fromEntries(map);
- let jsonResult=JSON.stringify(res);
-
+ let jsonResult=JSON.stringify(teamsWonBoth,null,2);
 
  fs.writeFileSync('/home/vamshi/Documents/JS-IPL-DATA/src/Public/output/5-won-toss-and-match.json',jsonResult,'utf8')
